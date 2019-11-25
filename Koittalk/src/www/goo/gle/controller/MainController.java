5a3149621799b67c7c51900f08da21070b2af82d
nononets <51473437+nononets@users.noreply.github.com>
2019-11-25 20:54:58 +0900

    2019-11-25 commit

---
+++ Koittalk/src/www/goo/gle/controller/MainController.java
@@ -0,0 +1,16 @@
+package www.goo.gle.controller;
+
+/**
+ * 
+ * @author 송은혁
+ * @since 2019-11-25
+ * @version 1.0
+ * @see
+ * 
+ */
+
+import javax.servlet.http.*;
+
+public interface MainController {
+	String execute(HttpServletRequest req, HttpServletResponse resp);
+}
---
+++ Koittalk/src/www/goo/gle/vo/Vo.java
@@ -0,0 +1,9 @@
+package www.goo.gle.vo;
+
+public class Vo {
+
+	public Vo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
